f21efa7111e4d8ff40911549ab8573eaa4a456a3
Fatal1ty770 <superigor.ru@mail.ru>
2021-05-15 23:12:48 +0500

    Add files via upload

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
-def main(request):
-    return render(request, 'mainapp/index.html')
 
 def products(request):
-    return render(request, 'mainapp/products.html')
-
-def contact(request):
-    return render(request, 'mainapp/contact.html')
+    links_menu = {'links': [
+        {'href': 'index', 'name': 'все'},
+        {'href': 'index', 'name': 'дом'},
+        {'href': 'index', 'name': 'офис'},
+        {'href': 'index', 'name': 'модерн'},
+        {'href': 'index', 'name': 'классика'}
+    ]}
+    return render(request, 'products.html',context=links_menu )
+# context=links_menu
