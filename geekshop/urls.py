3d8b5802365cf67ba7a8f4e17575f22f07ce500e
Fatal1ty770 <superigor.ru@mail.ru>
2021-05-20 22:12:56 +0500

    Add files via upload

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,6 +16,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from mainapp import urls
 from .views import contacts, main
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'geekshop'
 
@@ -23,7 +25,10 @@ urlpatterns = [
     path('', main, name='index'),
     path('admin/', admin.site.urls),
     path('contacts/', contacts, name='contacts'),
-    path('products/', include(urls), name='products'),
+    path('products/', include(urls, namespace='products')),
+    path('auth/', include('authapp.urls', namespace='auth')),
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- geekshop/views.py
+++ geekshop/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
+from mainapp.models import ProductCategory, Product
 
 
 def main(request):
+    products = Product.objects.all()[:4]
+
     context = {
         'slogan': 'Супер УДОБНЫЕ СТУЛЬЯ',
-        'topic': 'Тренды'
+        'topic': 'Тренды',
+        'products': products,
     }
     return render(request, 'index.html', context=context)
+
+
+
+    #
+    # content = {'title': title, 'products': products}
+    # return render(request, 'mainapp/index.html', content)
 
 
 def contacts(request):
